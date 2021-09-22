dfebfe68a4af1fc5a2e595e59645ad189598829a
David <davidchedrick@gmail.com>
2021-09-21 22:15:08 -0500

    commit tree 010c4c2f1f0893e3a87529c1d5c8eebdc681f44b

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word) {
+  return word.split('').reverse().join('');
 }
+
+function isPalindrome(word) {
+  const reverseWord = reverse(word);
+  return word === reverseWord;
+}
+
 
 /* 
   Add your pseudocode here
@@ -12,9 +18,28 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
